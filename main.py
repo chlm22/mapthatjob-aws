1d5ae9e1e8b13bec0a3016bc10df3b26739aadc4
Hae In Lee <haeinlee@umich.edu>
2026-08-16 08:44:48 -0400

    Add browser crash report hack

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from mangum import Mangum
 import os
 import psycopg2
@@ -8,31 +8,26 @@ app = FastAPI()
 
 DB_URL = os.environ.get("DATABASE_URL")
 
-def get_db_connection():
-    try:
-        conn = psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
-        return conn
-    except Exception as e:
-        print("Database connection error:", e)
-        return None
-
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the MapThatJob API!"}
 
 @app.get("/jobs")
 def get_jobs():
-    conn = get_db_connection()
-    if not conn:
-        raise HTTPException(status_code=500, detail="Could not connect to the database")
-    
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, title, company, location, latitude, longitude FROM jobs;")
-    jobs_data = cursor.fetchall()
-    
-    cursor.close()
-    conn.close()
-    
-    return {"jobs": jobs_data}
+    try:
+        # connection inside the route to catch the error
+        conn = psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, title, company, location, latitude, longitude FROM jobs;")
+        jobs_data = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return {"jobs": jobs_data}
+        
+    except Exception as e:
+        return {
+            "CRASH_REPORT": str(e), 
+            "URL_CHECK": str(DB_URL)
+        }
 
 handler = Mangum(app)
